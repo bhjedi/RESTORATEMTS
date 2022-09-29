7bdfb1c92a3376c53094299ceb19f5d221feba5e
Mehdi Boumzebra <boumzebramehdi@gmail.com>
2022-09-29 23:15:52 +0100

    create role & permission table

--- RESTORATE/config/app.php
+++ RESTORATE/config/app.php
@@ -141,6 +141,7 @@ return [
          */
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
