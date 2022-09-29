e25316b8ec534d9dea55582acd251455c11dc490
Mehdi Boumzebra <boumzebramehdi@gmail.com>
2022-09-29 18:15:58 +0100

    add necassary routes

--- RESTORATE/routes/web.php
+++ RESTORATE/routes/web.php
@@ -13,6 +13,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{   
+    /**
+     * Home Routes
+     */
+    Route::get('/', 'HomeController@index')->name('home.index');
+    Route::get('/restaurant/{id}', 'HomeController@index')->name('home.index');
+
+    Route::group(['middleware' => ['guest']], function() {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+        Route::post('/register', 'RegisterController@register')->name('register.perform');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+        Route::get('/adminRestaurantOwner', 'RestaurantOwnerController@index')->name('restaurant.owner');
+        Route::get('/adminRestaurantCustomer', 'RestaurantOwnerController@index')->name('restaurant.customer');
+
+
+    });
 });
